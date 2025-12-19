237290e36a82049d5d3c0bd52cc276b6439374e8
Jason BDEB <2474960@bdeb.qc.ca>
2025-12-19 17:56:04 -0500

    terminé la première partie de l'animation (rectangles coulissants)

--- GSAP-Bonus/src/main.js
+++ GSAP-Bonus/src/main.js
@@ -1,1 +1,21 @@
-import './style.css'
+import { SplitText } from 'gsap/all';
+import './style.css';
+import { gsap } from 'gsap';
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const backgroundList = ['.background1', '.background2', '.background3', '.background4'];
+const backgroundElements = backgroundList.map(element => document.querySelector(element));
+
+async function playAnimation() 
+{
+  await delay(900);
+
+  for (let i = 0; i < backgroundList.length; i++) 
+  {
+    await delay(100);
+    gsap.to(backgroundElements[i], { right: 0, duration: 2, ease: "power2.out" });
+  }
+};
+
+playAnimation();
